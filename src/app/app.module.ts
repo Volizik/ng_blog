a91d003e99949665cf0d0eb0481b9c46844213fc
kirill <apuc06@mail.ru>
2018-03-22 22:23:38 +0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { ArticlePageComponent } from './components/article-page/article-page.component';
+import { ArticleListComponent } from './components/article-list/article-list.component';
+import { ArticleComponent } from './components/article/article.component';
+import { AboutComponent } from './components/about/about.component';
+import { MaterialModule } from './material.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    ArticlePageComponent,
+    ArticleListComponent,
+    ArticleComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
