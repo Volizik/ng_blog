092d1a7eaf422c88633a1f33169d78e6e7521df6
kirill <apuc06@mail.ru>
2018-03-29 00:18:32 +0300

    верстка + адаптив

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { ArticlePageComponent } from './components/article-page/article-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainPageComponent},
+  {path: 'article/:id', component: ArticlePageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 @NgModule({
-  imports: [MatSidenavModule, MatButtonModule, MatIconModule],
-  exports: [MatSidenavModule, MatButtonModule, MatIconModule]
+  imports: [MatSidenavModule, MatButtonModule, MatIconModule, MatCardModule, MatPaginatorModule],
+  exports: [MatSidenavModule, MatButtonModule, MatIconModule, MatCardModule, MatPaginatorModule]
 })
 export class MaterialModule { }
